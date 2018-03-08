4bc7d5d7780b95e1507b407d7ada2677ebe7c5ee
yichung <ql3895619@gmail.com>
2018-03-08 09:51:21 +0800

    use multy argparser once

--- histogram.py
+++ histogram.py
@@ -1,9 +1,12 @@
-from argparse import ArgumentParser
+import argparse
 import cv2
 
-PrgumentParser.add_argument('filename', help='filename of image going to histogram equalization')
+parser = argparse.ArgumentParser()
+parser.add_argument('images', help='image filenames in YYYYmmDDHHMM format', nargs='*')
 args = parser.parse_args()
 
-img = cv2.imread(args.filename,0)
-equlized = cv2.equalizeHist(img)
-cv2.imwrite('equlized_' + args.filename, equlized)
+filenames = [filename for filename in args.images]
+for filename in filenames:
+    img = cv2.imread(filename,0)
+    equlized = cv2.equalizeHist(img)
+    cv2.imwrite('equlized_' + filename, equlized)
