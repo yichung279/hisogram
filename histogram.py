4728bf797b73a9cfb73eed1d8e8c0b2fab83d695
yichung <ql3895619@gmail.com>
2018-03-08 08:56:41 +0800

    add argparser

--- histogram.py
+++ histogram.py
@@ -1,6 +1,9 @@
+from argparse import ArgumentParser
 import cv2
-import numpy as np
 
-img = cv2.imread('test.jpg',0)
-equ = cv2.equalizeHist(img)
-cv2.imwrite('result.jpg', equ)
+PrgumentParser.add_argument('filename', help='filename of image going to histogram equalization')
+args = parser.parse_args()
+
+img = cv2.imread(args.filename,0)
+equlized = cv2.equalizeHist(img)
+cv2.imwrite('equlized_' + args.filename, equlized)
